5e2406954f467e51f568ab63f08452139a6136e9
Gregor Boltic <gregor.boltic@gmail.com>
2021-05-08 21:05:01 +0100

    Resized the chart with bootstrap, onClick animation done

--- static/js/chart-map-1.js
+++ static/js/chart-map-1.js
@@ -28,12 +28,9 @@ var mainChart = new Chart(mainChart, {
         },
         responsive: true,
         title: {
-            display: false,
+            display: true,
             text: 'Beer styles',
             fontSize: 20
-        },
-        animation: {
-            animateScale: false
         },
         onClick: secondChart
     }
@@ -44,14 +41,61 @@ var mainChart = new Chart(mainChart, {
 function secondChart(e){
     var activePoints = mainChart.getElementsAtEvent(e);
     var selectedIndex = activePoints[0]._index;
-    alert(this.data.labels[selectedIndex]);
-    var div = document.createElement('canvas');
-    div.id = "secondChart"
-    div.style.backgroundColor = "black"
-    div.width = 200
-    document.body.appendChild(div)
+    // alert(this.data.labels[selectedIndex]);
+
+    $("#moveable").animate({right: "25%"}, 1000)
+    console.log(ipa_sub)
+    console.log(Object.values(beer_styles_dictionary)) // test
+
+    setTimeout(function() {
+        var moveable = document.getElementById('moveable')
+        moveable.className = "col-md-6"
+        moveable.style = ""
+    
+        var div = document.createElement('div');
+        div.id = 'parent1'
+        div.className = "col-md-6"
+        document.getElementById('parent').appendChild(div)
+    
+        var canvas = document.createElement('canvas');
+        canvas.id = "secondChart";
+        canvas.style = "padding-top: 20px"
+        document.getElementById('parent1').appendChild(canvas);
+
+        var secondChart = document.getElementById("secondChart").getContext('2d');
+        var colorPalette = ['#d15e0e', '#f5751c', '#d6603a', '#2d4866', '#213651', '#17283c', '#797983']
+        var beerCount = [ipa, pale_ale, sour, belgian, lager, dark, other]
+        
+        var secondChart = new Chart(secondChart, {
+            type:'doughnut',
+            data: {
+                labels: ['IPA', 'Pale Ale', 'Sour', 'Belgian', 'Lager', 'Stout/Porter', 'Other'],
+                datasets: [{
+                    label: 'Beer style',
+                    data: beerCount,
+                    backgroundColor: colorPalette,
+                    borderWidth: 2,
+                    borderColor: 'white',
+                    hoverBorderWidth: 6,
+                    hoverBorderColor: 'white'
+                }]
+            },
+            options: {
+                legend: {
+                    display: true
+                },
+                responsive: true,
+                title: {
+                    display: true,
+                    text: 'Substyles',
+                    fontSize: 20
+                },
+            }
+        });
+    
+    }, 1300)
 }
-console.log(ipa_sub)
+
 
 //  MAP
 
